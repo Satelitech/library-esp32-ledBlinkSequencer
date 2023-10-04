76e176d750d2ab49187d20b2e1c361c5cf56ae51
Manuel Gonzalvez <manuelgonzalvez@gmail.com>
2023-10-04 13:11:54 -0300

    [ADD] 16-step mode con mismos comportamientos
    (destellos mas breves en modo "BANG")

--- lib/blinkSequencer/src/blinkSequencer.h
+++ lib/blinkSequencer/src/blinkSequencer.h
@@ -15,6 +15,12 @@ typedef enum
     ON            //* xxxxxxxx
 } blinkPattern;
 
+enum sequencerMode
+{
+    STEPS_8 = 0,
+    STEPS_16 = 1
+};
+
 typedef struct
 {
     blinkPattern periodType;
@@ -22,7 +28,8 @@ typedef struct
     uint8_t phase;
 } blinkContext;
 
-#define STEP_INTERVAL 125
+#define INTERVAL_8 125
+#define INTERVAL_16 62.5
 
 class sequencer
 {
@@ -31,9 +38,20 @@ public:
     sequencer()
         : _phase(0),
           _oldMillis(0),
-          _firstTrigger(false)
+          _firstTrigger(false),
+          _mode(sequencerMode::STEPS_8),
+          _interval(_mode == sequencerMode::STEPS_8 ? INTERVAL_8 : INTERVAL_16)
     {
     }
+    sequencer(sequencerMode mode)
+        : _phase(0),
+          _oldMillis(0),
+          _firstTrigger(false),
+          _mode(mode),
+          _interval(_mode == sequencerMode::STEPS_8 ? INTERVAL_8 : INTERVAL_16)
+    {
+    }
+
     void handler()
     {
         //* cuando recien lo seteo, reinicio y lo hago correr desde 0
@@ -43,7 +61,7 @@ public:
             this->_oldMillis = millis();
         }
         //* en ejecuciones siguientes, respeto el momento de la llamada del metodo
-        if (millis() - this->_oldMillis > STEP_INTERVAL)
+        if (millis() - this->_oldMillis > _interval)
         {
             this->_oldMillis = millis();
             this->incrementCounter();
@@ -53,11 +71,11 @@ public:
     void setPattern(blinkPattern _periodType, bool force = false)
     {
         if (!this->context.setPeriod || force)
-        {   
-            if(_periodType != this->context.periodType)
+        {
+            if (_periodType != this->context.periodType)
             {
-            this->_firstTrigger = true;
-            this->_phase = 0;
+                this->_firstTrigger = true;
+                this->_phase = 0;
             }
             this->context.periodType = _periodType;
             this->context.setPeriod = true;
@@ -97,33 +115,37 @@ public:
             break;
 
         case DOUBLE_BANG:
-            if (this->_phase == 0 || this->_phase == 4)
+            if (this->_phase == doubleIt(0) || this->_phase == doubleIt(4))
                 return true;
             return false;
             break;
 
         case TRI_BANG:
-            if (this->_phase == 0 || this->_phase == 3 || this->_phase == 6)
+            if (this->_phase == doubleIt(0) || this->_phase == doubleIt(3) || this->_phase == doubleIt(6))
                 return true;
             return false;
             break;
         case QUAD_BANG:
-            if (this->_phase == 0 || this->_phase == 2 || this->_phase == 4 || this->_phase == 6)
+            if (this->_phase == doubleIt(0) || this->_phase == doubleIt(2) || this->_phase == doubleIt(4) || this->_phase == doubleIt(6))
                 return true;
             return false;
             break;
+
         case BLINK:
-            if (this->_phase == 0 || this->_phase == 1)
+            // if (this->_phase ==  doubleIt(0) || this->_phase == doubleIt(1))
+            if (this->_phase < firstQuarter())
                 return true;
             return false;
             break;
         case LONG_BLINK:
-            if (this->_phase == 0 || this->_phase == 1 || this->_phase == 2 || this->_phase == 3)
+            // if (this->_phase ==  doubleIt(0) || this->_phase == doubleIt(1) || this->_phase == doubleIt(2)  || this->_phase == doubleIt(3))
+            if (this->_phase < firstHalf())
                 return true;
             return false;
             break;
         case DOUBLE_BLINK:
-            if (this->_phase == 0 || this->_phase == 1 || this->_phase == 4 || this->_phase == 5)
+            // if (this->_phase == doubleIt(0) || this->_phase == doubleIt(1) || this->_phase == doubleIt(4) || this->_phase == doubleIt(5))
+            if (this->_phase < firstQuarter() || (this->_phase > firstHalf() && this->_phase < thirdQuarter()))
                 return true;
             return false;
             break;
@@ -139,15 +161,34 @@ private:
     blinkContext context;
     unsigned long _oldMillis;
     bool _firstTrigger;
+    sequencerMode _mode;
+    long _interval;
+
+    uint8_t firstQuarter()
+    {
+        return (this->_mode == sequencerMode::STEPS_8 ? 2 : 4);
+    }
+    uint8_t thirdQuarter()
+    {
+        return (this->_mode == sequencerMode::STEPS_8 ? 6 : 12);
+    }
+    uint8_t firstHalf()
+    {
+        return (this->_mode == sequencerMode::STEPS_8 ? 4 : 8);
+    }
 
     void incrementCounter()
     {
         this->_phase++;
-        if (this->_phase > 7)
+        if (this->_phase > (this->_mode == sequencerMode::STEPS_8 ? 7 : 15))
         {
             this->context.setPeriod = false;
             this->_phase = 0;
         }
+    }
+    uint8_t doubleIt(uint8_t value)
+    {
+        return (this->_mode == sequencerMode::STEPS_8 ? (value) : (value * 2));
     }
 };
 
